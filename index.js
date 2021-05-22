1d072ac38f9b4ae811b673bd77b0a9d838e2a1ae
Fatim-Sohail <fatimso357@gmail.com>
2021-05-22 23:53:20 +0500

    feature1 implemented

---
+++ index.js
@@ -0,0 +1,31 @@
+// in JS, there are actually two ways to declare a variable. First one is with const and the next one is with let 
+
+const PI = 3.1416;
+let num = 1;
+let num2 = 2.5;
+let flag = false;
+
+// naming convention
+
+const NAME = 'Fatima'
+let firstNumber = 2;   // camel case
+let second_number = 32;  // pascal case
+
+// output
+console.log("hello world!");
+
+// operators *, -, +, / ===, **
+
+let thirdnumber = firstNumber + second_number;
+console.log(thirdnumber);
+
+// conditional statements
+
+if (firstNumber === second_number) {
+    console.log("Numbers are equal")
+} else {
+    console.log("Numbers are not equal")
+}
+
+
+
